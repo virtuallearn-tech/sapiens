3ebf5b723764860f5190c9e2539c82a6d5dca65f
Jordan Willian <jordan.willian.mp@gmail.com>
2025-12-20 16:03:31 -0300

    app working on firebase testing hosting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from "vite-tsconfig-paths";
+import tsconfigPaths from "vite-tsconfig-paths"
 import { VitePWA } from 'vite-plugin-pwa'
+// import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
   server: {
@@ -13,6 +14,7 @@ export default defineConfig({
   plugins: [
     react(), 
     tsconfigPaths(),
+    // basicSsl(),
     VitePWA({
       registerType: 'prompt',
       manifest: {
